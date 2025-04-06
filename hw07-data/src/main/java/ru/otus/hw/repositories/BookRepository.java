5b274c9d252f0d3b20dc186b9aee17c627d74bd3
borisowi1 <lborisovich@kazan.amfitel.ru>
2025-04-06 14:54:26 +0300

    HW07

--- hw07-data/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07-data/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -2,9 +2,11 @@ package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
@@ -12,4 +14,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
             "left join fetch b.comments left join fetch b.author")
     List<Book> findAll();
 
+    @Query("select b from Book b " +
+            "left join fetch b.genre left join fetch b.comments left join fetch b.author where b.id= :id")
+    Optional<Book>findById(@Param("id") Long id);
 }
