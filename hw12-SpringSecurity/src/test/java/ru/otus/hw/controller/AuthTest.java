e58e3907ee9156ebd777895d5f2852801cbca4c6
borisowi1 <lborisovich@kazan.amfitel.ru>
2025-06-05 22:55:10 +0300

    HW12 тесты

--- hw12-SpringSecurity/src/test/java/ru/otus/hw/controller/AuthTest.java
+++ hw12-SpringSecurity/src/test/java/ru/otus/hw/controller/AuthTest.java
@@ -3,31 +3,94 @@ package ru.otus.hw.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.otus.hw.auth.SecurityConfiguration;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.dto.AurhorDto;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.CommentDto;
+import ru.otus.hw.dto.GenreDto;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.services.AuthorService;
+import ru.otus.hw.services.BookService;
+import ru.otus.hw.services.CommentService;
+import ru.otus.hw.services.GenreService;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BooksController.class)
-@Import(SecurityConfiguration.class)
-public class PagesControllerTest {
+@WithMockUser("alisa")
+class AuthTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mvc;
 
-/*    @WithMockUser(
-            username = "admin",
-            authorities = {"ROLE_ADMIN"}
-    )*/
-    @Test
-    public void testAuthenticatedOnAdmin() throws Exception {
-        mockMvc.perform(get("/authenticated")
-                        .with(user("admin").authorities(new SimpleGrantedAuthority("ROLE_ADMIN"))))
+    @MockitoBean
+    private BookService bookService;
+    @MockitoBean
+    private BookConverter bookConverter;
+    @MockitoBean
+    private AuthorService authorService;
+    @MockitoBean
+    private GenreService genreService;
+    @MockitoBean
+    private CommentService commentService;
+
+
+    private List<BookDto> bookDtos = List.of(new BookDto(1, "War and Peace", new AurhorDto(), new GenreDto(), new ArrayList<CommentDto>()));
+    private List<AurhorDto> testAuthors = List.of(new AurhorDto(1, "Lev Tolstoj"));
+    private List<GenreDto> testGenreDtos = new ArrayList<>();
+
+      @Test
+    void shouldRenderListPageWithCorrectViewAndModelAttributes() throws Exception {
+        when(bookService.findAll()).thenReturn(bookDtos);
+        mvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void shouldRenderEditPageWithCorrectViewAndModelAttributes() throws Exception {
+        BookDto expectedBookDto = bookDtos.get(0);
+        when(bookService.findById(1L)).thenReturn(Optional.of(expectedBookDto));
+
+        mvc.perform(get("/edit").param("id", "1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldRenderErrorPageWhenBookNotFound() throws Exception {
+        when(bookService.findById(1L)).thenThrow(new EntityNotFoundException());
+        mvc.perform(get("/edit").param("id", "1"))
+                .andExpect(view().name("customError"));
+    }
+
+    @Test
+    void shouldSaveBookAndRedirectToContextPath() throws Exception {
+        BookDto expectedBookDto = bookDtos.get(0);
+        when(bookService.findById(1L)).thenReturn(Optional.of(expectedBookDto));
+        mvc.perform(post("/add")
+                        .param("title", "Anna K.")
+                        .param("authorId", "3")
+                        .param("genreId", "3")
+                   )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldRenderAddBook() throws Exception {
+        BookDto expectedBookDto = bookDtos.get(0);
+        when(bookService.findById(1L)).thenReturn(Optional.of(expectedBookDto));
+        when(authorService.findAll()).thenReturn(testAuthors);
+        mvc.perform(get("/add"))
+                .andExpect(status().isOk());
+      }
+
 }
--- hw12-SpringSecurity/src/test/java/ru/otus/hw/controller/BooksControllerTest.java
+++ hw12-SpringSecurity/src/test/java/ru/otus/hw/controller/BooksControllerTest.java
@@ -3,6 +3,7 @@ package ru.otus.hw.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.hw.converters.BookConverter;
@@ -24,10 +25,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BooksController.class)
+@WithMockUser("alisa")
 class BooksControllerTest {
 
     @Autowired
