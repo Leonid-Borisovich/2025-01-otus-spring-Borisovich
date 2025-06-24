b1ce174cb083df616a9f9b302da856c93ad3e0a3
borisowi1 <lborisovich@kazan.amfitel.ru>
2025-06-24 12:51:39 +0300

    HW33 run app in docker container

---
+++ hw-33-docker/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into authors(full_name)
+values ('Агата Кристи!'), ('О.Генри'), ('Джек Лондон');
+
+insert into genres(name)
+values ('Детектив'), ('Комедия'), ('Приключения');
+
+insert into books(title, author_id, genre_id)
+values ('10 негритят', 1, 1), ('Короли и капуста', 2, 2), ('Зов предков', 3, 3);
+
+insert into comments(text, book_id)
+values ('Агата Кристи - это классика детектива! Советую!', 1),
+       ('А какого современного автора посоветуйте?', 1),
+       ('Почитайте Б.Акунина!', 1);
